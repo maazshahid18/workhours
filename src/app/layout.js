2800f7029f7a92a3e573418e840a44288396d65c
maaz <mzperwez786@gmail.com>
2025-12-09 16:37:01 +0530

    changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,7 +28,7 @@ export const viewport = {
   initialScale: 1,
   maximumScale: 1,
   userScalable: false,
-  themeColor: "#667eea",
+  themeColor: "#FFFFFF",
 };
 
 export default function RootLayout({ children }) {
